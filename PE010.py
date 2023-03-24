930a5a0d023fef01e693edda3a470a0801959591
Jean Maccou <49556588+Nexowo@users.noreply.github.com>
2023-03-24 09:01:31 +0100

    Update PE010.py

--- PE010.py
+++ PE010.py
@@ -2,22 +2,30 @@
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
+
+(Enhanced thanks to C. Obrecht)
 """
 
-s=0
+def exaustive_primal_test(number:int, primes:list[int]) -> bool:
+    limit = number ** (1/2)
+    
+    for p in primes:
+        if p > limit:
+            return True
+        if number % p == 0:
+            return False
+    
+    return True
 
-def list_primes(x : int):
+def get_n_first_primes(n:int)-> list[int]:
     primes = [2]
-    for i in range(2, x+1):
-        for elem in primes:
-            if i%elem == 0:
-                break
-            elif elem == primes[-1]:
-                primes.append(i)
-                break
+    for i in range(3,n+1,2):
+
+        if exaustive_primal_test(i, primes):
+            primes.append(i)
+
     return primes
 
-for elem in list_primes(2000000):
-    s+=elem
+if __name__ == "__main__":
+    print(sum(get_n_first_primes(2000000)))
 
-print(s)
