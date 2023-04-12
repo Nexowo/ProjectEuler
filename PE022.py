28482d2baf253d262d666c5573cb4b2d7dcfdd4d
Jean Maccou <49556588+Nexowo@users.noreply.github.com>
2023-04-12 08:48:10 +0200

    Create PE022.py

---
+++ PE022.py
@@ -0,0 +1,56 @@
+"""
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+"""
+path = ...
+
+alpha = {
+    "A" : 1,
+    "B" : 2,
+    "C" : 3,
+    "D" : 4,
+    "E" : 5,
+    "F" : 6,
+    "G" : 7,
+    "H" : 8,
+    "I" : 9,
+    "J" : 10,
+    "K" : 11,
+    "L" : 12,
+    "M" : 13,
+    "N" : 14,
+    "O" : 15,
+    "P" : 16,
+    "Q" : 17,
+    "R" : 18,
+    "S" : 19,
+    "T" : 20,
+    "U" : 21,
+    "V" : 22,
+    "W" : 23,
+    "X" : 24,
+    "Y" : 25,
+    "Z" : 26
+}
+
+def score(name : str):
+    s = 0
+    for letter in name:
+        s+= alpha[letter]
+    return s
+
+with open(path, 'r') as f:
+    st = f.read()
+    f.close()
+
+st = st.replace('"','')
+st = st.split(',')
+st.sort()
+
+tot = 0
+for i in range(len(st)):
+    tot+= (i+1)*score(st[i])
+print(tot)
